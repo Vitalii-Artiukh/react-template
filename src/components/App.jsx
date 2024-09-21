1c8e1160f4132dac4ca5126acbe746d5e39067d5
Vitalii Artiukh <radial_car@yahoo.com>
2024-09-21 11:11:08 +0300

    valid

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,7 @@
-import { useState } from 'react'
-import reactLogo from '../assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from 'react';
+import reactLogo from '../assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 // function App() {
 //   const [count, setCount] = useState(0)
@@ -44,17 +43,23 @@ import './App.css'
 //   );
 // };
 
-import Product from "./Product";
+import Product from './Product';
 
-export default function App() {
+export default function App(props) {
   return (
     <div>
-      <h1>Products</h1>
+      <h1>Best selling</h1>
 
-      <Product />
-      <Product />
-      <Product />
+      <Product
+        name="Tacos With Lime"
+        imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
+        price={10.99}
+      />
+      <Product
+        name="Fries and Burger"
+        imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
+        price={14.29}
+      />
     </div>
   );
 }
-
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,10 +1,14 @@
-export default function Product() {
-  const price = 888;
+export default function Product({
+  name,
+  imgUrl = 'https://dummyimage.com/640x480/2a2a2a/ffffff&text=Product+image+placeholder',
+  price,
+}) {
   return (
     <div>
-      <h2>Tacos</h2>
-      <img src="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640" alt="Tacos With Lime" width="640" />
-	  <p>Price: {price} credits</p>
+      {/* <Product {...props} /> */}
+      <h2>{name}</h2>
+      <img src={imgUrl} alt={name} width={480} />
+      <p>Price: {price} credits</p>
     </div>
   );
-};
+}
